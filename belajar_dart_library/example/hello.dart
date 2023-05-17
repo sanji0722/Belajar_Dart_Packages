fcfb6edd5526261d48224dcf92e4c4d7d54d98d4
sanji2207 <saepullohepul565@gmail.com>
2023-05-18 06:49:15 +0700

    Semangat

---
+++ belajar_dart_library/example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main() {
+  print(sayhello("Toni"));
+
+  var data1 = Konsumen();
+  var data2 = Kategori();
+  var data3 = Produk();
+}
---
+++ belajar_dart_library/lib/hello.dart
@@ -0,0 +1,4 @@
+library belajar_dart_library;
+
+export 'src/sayhello.dart';
+export 'src/konsumen.dart' show Konsumen, Kategori, Produk;
